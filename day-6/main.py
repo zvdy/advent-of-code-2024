c8853c61eec423e09e7d6e0b33a73d6ee0ddcdb0
zvdy <cristian.030902@gmail.com>
2024-12-09 19:41:13 +0100

    day6 py solution

--- day-6/main.py
+++ day-6/main.py
@@ -1,49 +1,104 @@
-def load_maze(file_path):
-    with open(file_path, 'r') as file:
-        return [list(line.strip()) for line in file.readlines()]
+class Guard:
 
-def find_guard(maze):
-    DIRECTIONS = {'^', '>', 'v', '<'}
-    for r, row in enumerate(maze):
-        for c, cell in enumerate(row):
-            if cell in DIRECTIONS:
-                guard_pos = (r, c)
-                guard_dir = cell
-                maze[r][c] = '.'  # Clear the starting position
-                return guard_pos, guard_dir
-    return None, None
+    change_dir_map = {'u': 'r', 'r': 'd', 'd': 'l', 'l': 'u'}
+    dir_vector = {'u': [-1, 0], 'r': [0, 1], 'd': [1, 0], 'l': [0, -1]}
 
-def simulate_guard_movement(maze, guard_pos, guard_dir):
-    DIRECTIONS = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
-    TURN_RIGHT = {'^': '>', '>': 'v', 'v': '<', '<': '^'}
-    
-    rows, cols = len(maze), len(maze[0])
-    visited_positions = set()
-    visited_positions.add(guard_pos)
-    
-    while True:
-        next_r, next_c = guard_pos[0] + DIRECTIONS[guard_dir][0], guard_pos[1] + DIRECTIONS[guard_dir][1]
-        
-        if not (0 <= next_r < rows and 0 <= next_c < cols):  # Guard leaves the mapped area
-            break
-        
-        if maze[next_r][next_c] == '#':  # Obstacle ahead, turn right
-            guard_dir = TURN_RIGHT[guard_dir]
-        else:  # Move forward
-            guard_pos = (next_r, next_c)
-            visited_positions.add(guard_pos)
-    
-    return len(visited_positions)
+    def __init__(self, start_coordinates: list[int]):
+        self.init_position = start_coordinates.copy()
+        self.restart_loop_tracker()
+        self.main_path = set()
+        self.positions_seen = 1
+        self.possible_move = None
+        self.in_bounds = True
 
-def main():
-    file_path = 'input.txt'
-    maze = load_maze(file_path)
-    guard_pos, guard_dir = find_guard(maze)
-    if guard_pos and guard_dir:
-        distinct_positions = simulate_guard_movement(maze, guard_pos, guard_dir)
-        print(distinct_positions)
-    else:
-        print("Guard not found in the maze.")
+    def change_dir(self):
+        self.dir = self.change_dir_map[self.dir]
 
-if __name__ == "__main__":
-    main()
+    def find_next_move(self):
+        self.possible_move = [self.position[x] + self.dir_vector[self.dir][x]
+                              for x in range(2)]
+        return self.possible_move
+
+    def accept_move(self, next_tile: str, part_1: bool = True):
+        self.position = self.possible_move
+        if part_1:
+            self._count_if_new(next_tile == '.')
+            self.track_main_path(tuple(self.position))
+        else:
+            return self.track_potential_loop_path(tuple(self.position))
+
+    def _count_if_new(self, new: bool):
+        self.positions_seen += new
+
+    def restart_loop_tracker(self):
+        self.position = self.init_position.copy()
+        self.dir = 'u'
+        self.position_visits = set([(self.position[0],
+                                     self.position[1],
+                                     self.dir)])
+        self.in_bounds = True
+
+    def track_main_path(self, coordinates: tuple[int]):
+        if coordinates != tuple(self.init_position):
+            self.main_path.add(coordinates)
+
+    def track_potential_loop_path(self, coordinates: tuple[int]):
+        cur = coordinates + tuple(self.dir)
+        if cur not in self.position_visits:
+            self.position_visits.add(cur)
+            return False
+        return True
+
+
+class AdventDay6:
+
+    def __init__(self):
+        self.room_map = {}
+        with open('input.txt', 'r') as f:
+            for i, line in enumerate(f.readlines()):
+                for j, char in enumerate(line):
+                    if char == '\n':
+                        continue
+                    if char == '^':
+                        self.guard = Guard([i, j])
+                        char = 'S'
+                    self.room_map[(i, j)] = char
+        self.part1 = 0
+        self.part2 = 0
+
+    def get_patrol_route(self, part_1: bool = True):
+        while self.guard.in_bounds:
+            try:
+                next_tile = self.room_map[tuple(self.guard.find_next_move())]
+                if next_tile != '#':
+                    loop = self.guard.accept_move(next_tile, part_1)
+                    self.room_map[tuple(self.guard.position)] = '!'
+                    if not part_1 and loop:
+                        self.part2 += 1
+                        break
+                else:
+                    self.guard.change_dir()
+            except KeyError:
+                self.guard.in_bounds = False
+        if part_1:
+            self.part1 = self.guard.positions_seen
+
+    def find_potential_loops(self):
+        for obstical_potential in self.guard.main_path:
+            self.room_map[obstical_potential] = '#'
+            self.guard.restart_loop_tracker()
+            self._reset_map()
+            self.get_patrol_route(part_1=False)
+            self.room_map[obstical_potential] = '.'
+
+    def _reset_map(self):
+        for key, val in self.room_map.items():
+            if val == '!':
+                self.room_map[key] = '.'
+
+
+if __name__ == '__main__':
+    day6 = AdventDay6()
+    day6.get_patrol_route()
+    day6.find_potential_loops()
+    print(day6.part1, day6.part2)
